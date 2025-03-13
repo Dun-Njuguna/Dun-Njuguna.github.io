d9410732da5de83e94f9ca13f4781180db9e453a
Duncan Kariuki <duncankariuki60@gmail.com>
2025-03-13 23:47:27 +0300

    Updates

---
+++ static/js/278.6110e349.chunk.js
@@ -0,0 +1,75 @@
+"use strict";(self.webpackChunkDuncan_portfolio=self.webpackChunkDuncan_portfolio||[]).push([[278],{5639:(e,i,t)=>{t.d(i,{$:()=>o});const r=(0,t(4905).Ay)("button")`
+  background: ${e=>e.color||"#2e186a"};
+  color: ${e=>e.color?"#2E186A":"#fff"};
+  font-size: 1rem;
+  font-weight: 700;
+  width: 100%;
+  border: 1px solid #edf3f5;
+  border-radius: 4px;
+  padding: 13px 0;
+  cursor: pointer;
+  margin-top: 0.625rem;
+  max-width: 180px;
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0 16px 30px rgb(23 31 114 / 20%);
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: #fff;
+    border: 1px solid rgb(255, 130, 92);
+    background-color: rgb(255, 130, 92);
+  }
+`;var n=t(579);const o=e=>{let{color:i,children:t,onClick:o}=e;return(0,n.jsx)(r,{color:i,onClick:o,children:t})}},1825:(e,i,t)=>{t.d(i,{A:()=>c});t(5043);var r=t(2382),n=t(2646),o=(t(4430),t(9078),t(579));const s=e=>{const{className:i,style:t,onClick:r}=e;return(0,o.jsx)("div",{className:i,style:{...t,display:"flex",alignItems:"center",justifyContent:"center",left:"-0px",zIndex:1,width:"40px",height:"40px",backgroundColor:"#18216d",borderRadius:"50%"},onClick:r})},d=e=>{const{className:i,style:t,onClick:r}=e;return(0,o.jsx)("div",{className:i,style:{...t,display:"flex",alignItems:"center",justifyContent:"center",right:"-0px",zIndex:1,width:"40px",height:"40px",backgroundColor:"#18216d",borderRadius:"50%"},onClick:r})},l={dots:!0,lazyLoad:!0,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1,initialSlide:0,arrows:!0,prevArrow:(0,o.jsx)(s,{}),nextArrow:(0,o.jsx)(d,{})},c=e=>{let{images:i}=e;return 1===i.length?(0,o.jsx)(n.A,{src:i[0],width:"100%",height:"100%"}):(0,o.jsx)(r.A,{...l,children:i.map(((e,i)=>(0,o.jsx)("div",{children:(0,o.jsx)(n.A,{src:e,width:"84%",height:"600px"})},i)))})}},3282:(e,i,t)=>{t.r(i),t.d(i,{default:()=>h});var r=t(4647),n=t(2444),o=t(4703),s=t(4117),d=t(5639),l=t(2646),c=t(2026),a=t(1825),x=t(579);const h=(0,s.CI)()((e=>{let{images:i,title:t,content:s,github_url:h,modules:p,section:m,button:g,t:j,id:u,direction:f}=e;return(0,x.jsx)(c.Fe,{children:(0,x.jsx)(o.zW,{direction:f,triggerOnce:!0,children:(0,x.jsxs)(c.yX,{justify:"space-between",align:"middle",id:u,direction:f,children:[(0,x.jsx)(r.A,{lg:11,md:11,sm:12,xs:24,children:(0,x.jsx)(a.A,{images:i})}),(0,x.jsx)(r.A,{lg:11,md:11,sm:11,xs:24,children:(0,x.jsxs)(c.HD,{children:[(0,x.jsx)(c.j_,{children:j(t)}),(0,x.jsx)(c.UC,{children:j(s)}),"right"===f?(0,x.jsx)(c.eM,{children:"object"===typeof g&&g.map(((e,i)=>(0,x.jsx)(d.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("contact"),children:j(e.title)},i)))}):(0,x.jsx)(c.xv,{children:(0,x.jsx)(n.A,{justify:"space-between",children:"object"===typeof m&&m.map(((e,i)=>(0,x.jsxs)(r.A,{span:11,children:[(0,x.jsx)(l.A,{src:e.icon,width:"60px",height:"60px"}),(0,x.jsx)(c.j_,{children:j(e.title)}),(0,x.jsx)(c.dG,{children:j(e.content)})]},i)))})})]})})]})})})}))},2026:(e,i,t)=>{t.d(i,{Fe:()=>o,HD:()=>l,UC:()=>s,ck:()=>p,dG:()=>x,eM:()=>h,j_:()=>a,xv:()=>c,yX:()=>d});var r=t(2444),n=t(4905);const o=(0,n.Ay)("section")`
+  position: relative;
+  padding: 5rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,s=(0,n.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+      line-height: 2;
+  color: #555;
+`,d=(0,n.Ay)(r.A)`
+  flex-direction: ${e=>{let{direction:i}=e;return"left"===i?"row":"row-reverse"}};
+`,l=(0,n.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,c=(0,n.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,a=(0,n.Ay)("h6")`
+  font-size: 15px;
+  margin-bottom: 0.5rem;
+  color: #222;
+`,x=(0,n.Ay)("p")`
+    line-height: 2;
+  color: #555;
+`,h=(0,n.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`,p=n.Ay.img`
+  width: 160%;
+  height: 160%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
+`}}]);
+//# sourceMappingURL=278.6110e349.chunk.js.map
